cf2b4881a4931f843c90b05023b04961fe3c0868
chemartinhurtado <jose.martin95@hotmail.es>
2026-06-08 11:50:50 +0200

    Day 7 in progress

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,6 @@
+def my_function():
+    print("Hello World")
+    print("Bye")
+
+my_function()
+
--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,43 @@
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+placeholder = ""
+word_length = len(chosen_word)
+display = ""
+correct_letters = []
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+# TODO-1: - Use a while loop to let the user guess again.
+
+
+while display != chosen_word:
+    guess = input("Guess a letter: ").lower()
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+
+
+if display == chosen_word:
+    print("You Won!")
+
+
+
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+
