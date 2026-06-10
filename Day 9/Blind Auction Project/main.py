9b563cbf638aff6f30a4e9282de96d84182edca9
chemartinhurtado <jose.martin95@hotmail.es>
2026-06-10 17:44:06 +0200

    Day 8 completed

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,25 @@
+from idlelib.pyshell import restart_line
+
+import art
+print(art.logo)
+restart = True
+
+while restart:
 # TODO-1: Ask the user for input
+    name = input("What is your name?:")
+    price = input("What is your bid?: $ ")
+
 # TODO-2: Save data into dictionary {name: price}
+    bidders = {
+    }
+
 # TODO-3: Whether if new bids need to be added
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'")
+    if other_bidders == "yes":
+        print("\n" * 100)
+
+    else:
+        restart = False
+        print(bidders)
+
 # TODO-4: Compare bids in dictionary
-
-
