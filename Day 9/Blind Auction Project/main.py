afe9ef09be34bcae56dcc7696aba29c8edfcb976
jmmartin <jose.martin95@hotmail.es>
2026-06-18 16:28:18 +0200

    Update de proyecto Dia5

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,24 +2,38 @@ from idlelib.pyshell import restart_line
 
 import art
 print(art.logo)
-restart = True
-
-while restart:
 # TODO-1: Ask the user for input
-    name = input("What is your name?:")
-    price = input("What is your bid?: $ ")
-
 # TODO-2: Save data into dictionary {name: price}
-    bidders = {
-    }
-
 # TODO-3: Whether if new bids need to be added
-    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'")
-    if other_bidders == "yes":
-        print("\n" * 100)
 
-    else:
-        restart = False
-        print(bidders)
+
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?:")
+    price = int(input("What is your bid?: $ "))
+    bids[name] = price
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n").lower()
+    if other_bidders == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif other_bidders == "yes":
+        print("\n" * 20 )
+
 
 # TODO-4: Compare bids in dictionary
+
+
